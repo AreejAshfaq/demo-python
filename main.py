60612b3a5516ab47c9648a5407934f6f038e454d
AreejAshfaq <areejch15222@gmail.com>
2024-10-17 04:47:18 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,59 @@
+#variable
+a=5
+b=6
+#opreator
+#arithmatic opreator
+
+
+print(a-b)
+print(a+b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b)
+print("i will start  learning python")
+
+#relational opraetor
+a=100
+b=900
+print(a==b)#False
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a!=b)
+print(a<=b)
+
+#assignment opreator
+num=10
+num=num*10
+print(num)
+num2=50
+num=num2+40
+print(num2)
+a=60
+a=a%70
+print(a)
+
+#logical opraetors
+#AND ,OR ,NOT
+#NOT OPRAETOR
+a=50
+b=90
+print(not True)
+print( not a>b)
+
+#AND opraetor
+num1=True
+num2=False
+print("and opraetor:",num1 and num2)
+
+#OR opreator
+
+num1=True
+num2=False
+print("OR opraetor:",num1 or num2)
+
+
+#how to find data type
+name="areej"
+print(type(name))
